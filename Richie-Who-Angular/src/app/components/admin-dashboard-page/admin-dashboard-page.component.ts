0bda09524da76ea9605d7c484ce248ca307ef140
Samuele Deriu <deriusamuele@gmail.com>
2025-08-15 19:13:22 +0200

    chore: updated book.service, modified the functions to convert the data received from the backend in simple object for the components

--- Richie-Who-Angular/src/app/components/admin-dashboard-page/admin-dashboard-page.component.ts
+++ Richie-Who-Angular/src/app/components/admin-dashboard-page/admin-dashboard-page.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthServiceService } from '../../services/auth-service.service';
+import { BookService } from '../../services/book.service';
+import { Book } from '../../models/book.model';
+
 @Component({
   selector: 'app-admin-dashboard-page',
   imports: [],
   templateUrl: './admin-dashboard-page.component.html',
   styleUrl: './admin-dashboard-page.component.scss',
 })
-export class AdminDashboardPageComponent {
-  constructor(private auth: AuthServiceService) {}
+export class AdminDashboardPageComponent implements OnInit {
+  books: Book[] = [];
+
+  constructor(
+    private auth: AuthServiceService,
+    private bookService: BookService
+  ) {}
+
+  ngOnInit(): void {
+    this.loadBooks();
+  }
+  loadBooks() {
+    this.bookService.getBooks().subscribe((data: Book[]) => {
+      this.books = data;
+    });
+  }
 
   onLogout() {
     this.auth.signout();
--- Richie-Who-Angular/src/app/services/book.service.ts
+++ Richie-Who-Angular/src/app/services/book.service.ts
@@ -2,103 +2,124 @@ import { inject, Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { TokenService } from './token.service';
 import { Book } from '../models/book.model';
+import { Observable, map } from 'rxjs';
+
 @Injectable({
   providedIn: 'root',
 })
 export class BookService {
-  //endpoint firestore
+  /*ENDPOINT Firestore */
   private baseUrl =
     'https://firestore.googleapis.com/v1/projects/richiewho/databases/(default)/documents/books';
 
   constructor(private http: HttpClient, private tokenService: TokenService) {}
 
-  getBooks() {
-    const token = this.tokenService.getAccessToken(); //retrieve saved token
-    const headers = new HttpHeaders({
-      //create a obj for HTTP headers
-      Authorization: `Bearer ${token}`,
-    });
-
-    return this.http.get(this.baseUrl, { headers }); // req to endpoint firestore
+  /* GET BOOKS
+    this function return an Book[]. 
+    We use map for each element from firestore for setting in Book obj the result. 
+    In this way the component read a Book array ready to use.
+  */
+  getBooks(): Observable<Book[]> {
+    return this.http
+      .get<any>(this.baseUrl, { headers: this.getAuthHeaders() })
+      .pipe(
+        map(
+          (res) =>
+            res.documents?.map((doc: any) => this.mapFirestoreDocToBook(doc)) || //mapFirestoreDocToBook convert data from firestore (fields.stringValue) in Book
+            []
+        )
+      );
   }
 
-  getBook(id) {}
+  /* GET SINGLE BOOK
+  this funcion return an "Observable<Book>" single book that we retray from firestore and
+   we convert in Book using the function mapFirestoreDocToBook.
+*/
+  getBook(id: string): Observable<Book> {
+    const url = `${this.baseUrl}/${id}`; // endpoint firestore
 
-  createBook(book: {
-    title: string;
-    author: string;
-    description: string;
-    year: number;
-  }) {
-    const token = this.tokenService.getAccessToken();
+    return this.http
+      .get<any>(url, { headers: this.getAuthHeaders() })
+      .pipe(map((doc) => this.mapFirestoreDocToBook(doc)));
+  }
 
-    /* 
-      header with Bearer it's used by 
-      firestore to check if the user is logged in
-     */
-    const headers = new HttpHeaders({
-      //create a obj for HTTP headers
-      Authorization: `Bearer ${token}`,
-    });
+  /* CREATE A NEW BOOK
+  This function create a new book.
+  Create a body in firestore REST format "fields.stringValue".
+  Executes POST in /documents/books for create a new book
+  Receives the new book created
 
-    // body in format "fields" for Firestore
+*/
+  createBook(book: Book): Observable<Book> {
     const body = {
+      //standard format for firestore
       fields: {
         title: { stringValue: book.title },
         author: { stringValue: book.author },
+        description: { stringValue: book.description },
         year: { integerValue: book.year },
       },
     };
 
-    return this.http.post(this.baseUrl, body, { headers }); // create a document into the collection "books"
+    return this.http
+      .post<any>(this.baseUrl, body, { headers: this.getAuthHeaders() }) //executes a POST for create a new book
+      .pipe(map((doc) => this.mapFirestoreDocToBook(doc))); // converts the received data into a book
   }
 
-  updateBook(
-    id: string,
-    book: {
-      title?: string;
-      author?: string;
-      description?: string;
-      year?: number;
-    }
-  ) {
-    const token = this.tokenService.getAccessToken(); //retrieve saved token
-    const headers = new HttpHeaders({
-      //create a obj for HTTP headers
-      Authorization: `Bearer ${token}`,
-    });
-
+  /* UPDATE A BOOK
+    This function update a book
+    Building a body.field and use only the data in book for dont deleted another dataCreate an update mask. Firestore uses it to know which data is being updated, without risking data loss.
+    ex: if book{title: 'x', year: 2020} so updateMask ->updateMask.fieldPaths=title&updateMask.fieldPaths=year
+    In the end map the answer in book
+  */
+  updateBook(id: string, book: Partial<Book>): Observable<Book> {
     const body = {
+      // format for Firestore body.field
       fields: {
-        // ...((condition) && .. means if book.title exist you can replace)
-        ...(book.title && book.title && { title: { stringValue: book.title } }),
-        ...(book.author && { title: { stringValue: book.author } }),
-        ...(book.description && { title: { stringValue: book.description } }),
-        ...(book.year && book.year && { title: { stringValue: book.year } }),
+        ...(book.title && { title: { stringValue: book.title } }),
+        ...(book.author && { author: { stringValue: book.author } }),
+        ...(book.description && {
+          description: { stringValue: book.description },
+        }),
+        ...(book.coverUrl && { coverUrl: book.coverUrl }),
+        ...(book.createdAt && { year: { integerValue: book.createdAt } }),
       },
     };
-    /* 
-      With updateMask you can update the indicated fields
-      without deleting the unindicated ones. 
-      Without updateMask the unindicated fields will be deleted.
-     */
-    const updateMask = Object.keys(book)
+
+    const updateMask = Object.keys(book) // Firestore use updateMask for to know wich data update
       .map((k) => `updateMask.fieldPaths=${k}`)
       .join('&');
 
-    const url = `${this.baseUrl}/${id}?${updateMask}`; //I build URL endpoints for update the book
-
-    return this.http.patch(url, body, { headers }); //patch -> HTTP method used to partially update a document.
+    const url = `${this.baseUrl}/${id}?${updateMask}`; //endpoit firestore
+    return this.http
+      .patch<any>(url, body, { headers: this.getAuthHeaders() }) //PATCH with updateMask allows partial updates without loss data
+      .pipe(map((doc) => this.mapFirestoreDocToBook(doc))); // return a book
   }
 
-  deleteBook(id: string) {
-    const token = this.tokenService.getAccessToken(); //retrieve saved token
-    const headers = new HttpHeaders({
-      //create a obj for HTTP headers
-      Authorization: `Bearer ${token}`,
-    });
-    const url = `${this.baseUrl}/${id}`; //I build URL endpoints for delete
+  /* DELETE A BOOK
+  this function delete a book and retur and observable  
+*/
+  deleteBook(id: string): Observable<void> {
+    const url = `${this.baseUrl}/${id}`; //endpoint Firestore
 
-    return this.http.delete(url, { headers }); //delete-> method HTTP
+    return this.http.delete<void>(url, { headers: this.getAuthHeaders() }); //delete-> method HTTP
+  }
+
+  /* BUILD HTTPHeaders */
+  private getAuthHeaders(): HttpHeaders {
+    const token = this.tokenService.getAccessToken(); //retrieve saved token -> string|null
+    return new HttpHeaders({ Authorization: `Bearer ${token}` }); // return a HttpHeaders object with new token. Bearer is the standard for send a JWT/ID. if toke is null ->Authorization: "Bearer null"
+  }
+
+  /*CONVERT DOC IN BOOK */
+  private mapFirestoreDocToBook(doc: any): Book {
+    return {
+      id: doc.name.split('/').pop(),
+      title: doc.fields?.title?.stringValue || '',
+      author: doc.fields?.author?.stringValue || '',
+      description: doc.fields?.description?.stringValue || '',
+      coverUrl: doc.fields?.coverUrl?.stringValue || '',
+      createdAt: parseInt(doc.fields?.createdAt?.integerValue || '0', 10),
+    };
   }
 }
